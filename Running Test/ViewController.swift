757aab76ce4c8b394fae14bdec60705fbf7e9b23
Debbie Neubieser <debbieneubieser@debbies-macbook.attlocal.net>
2017-06-05 13:38:04 -0700

    Initial Commit

---
+++ Running Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Running Test
+//
+//  Created by Debbie Neubieser on 6/5/17.
+//  Copyright © 2017 Craig Neubieser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
