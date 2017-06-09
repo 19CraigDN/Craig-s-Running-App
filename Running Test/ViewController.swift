5b7aae258b48a53c9e9f0fb341e672e5f5c74d33
15craign <15craign@alumni.harker.org>
2017-06-08 19:40:44 -0700

    Second Commit

--- Running Test/ViewController.swift
+++ Running Test/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var placeNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        placeNameLabel.text = "Default Text"
+    }
+    
 }
 
